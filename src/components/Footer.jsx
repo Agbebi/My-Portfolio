70b02925f11a36d58d238038e6c9d8423d7d4636
Agbebi Timothy <agbebitimothy8@gmail.com>
2025-10-12 10:18:31 +0100

    Fixed the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='sticky bottom-0' style={{ textAlign: 'center', padding: '1rem 0', background: '#f5f5f5' }}>
+        <footer className='z-9 sticky bottom-0' style={{ textAlign: 'center', padding: '1rem 0', background: '#f5f5f5' }}>
             <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
         </footer>
     );
