2149235b8a5375e5b41b48399692ced643627690
Agbebi Timothy <agbebitimothy8@gmail.com>
2025-10-18 05:17:57 +0100

    Added Tasking Landing page website

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -2,7 +2,7 @@ const projectdata = [
     {
         "id": 1,
         "title": "Portfolio Website",
-        "description": "This is a description of project five.",
+        "description": "This is a description of project one.",
         "image": "path/to/image5.jpg",
         "link": "https://picsum.photos/300/200?random=5",
         "skills_used": ["Javascript", "React", "HTML", "CSS"],
@@ -11,7 +11,7 @@ const projectdata = [
     {
         "id": 2,
         "title": "To do List App",
-        "description": "This is a description of project three.",
+        "description": "This is a description of project two.",
         "image": "path/to/image3.jpg",
         "link": "https://picsum.photos/300/200?random=3",
         "skills_used": ["Ejs", "Javascript", "Node.Js", "Express", "CSS", "MongoDB"],
@@ -20,7 +20,7 @@ const projectdata = [
     {
         "id": 3,
         "title": "Driving School Website",
-        "description": "This is a description of project four.",
+        "description": "This is a description of project three.",
         "image": "path/to/image4.jpg",
         "link": "https://picsum.photos/300/200?random=4",
         "skills_used": ["Javascript", "Node.Js", "Express", "HTML", "CSS"],
@@ -28,17 +28,26 @@ const projectdata = [
     },
     {
         "id": 4,
+        "title": "Tasking App",
+        "description": "This is a description of project four.",
+        "image": "path/to/image6.jpg",
+        "link": "https://picsum.photos/300/200?random=6",
+        "skills_used": ["HTML5", "CSS", "Javascript"],
+        "value": "https://agbebi.github.io/Tasking-Landing-Page/"
+    },
+    {
+        "id": 5,
         "title": "Carousel Design",
-        "description": "This is a description of project one.",
+        "description": "This is a description of project five.",
         "image": "path/to/image1.jpg",
         "link": "https://picsum.photos/300/200?random=1",
         "skills_used": ["Javascript", "HTML", "CSS"],
         "value": "https://agbebi.github.io/Carousel-Design/"
     },
     {
-        "id": 5,
+        "id": 6,
         "title": "Newsletter Signup Form",
-        "description": "This is a description of project two.",
+        "description": "This is a description of project six.",
         "image": "path/to/image2.jpg",
         "link": "https://picsum.photos/300/200?random=2",
         "skills_used": ["Javascript", "Node.Js", "Express", "HTML", "CSS"],
